6e632dc91dbdaef60270a6ce0d5697f6b358e081
Wharfiggle <elijahsouthman@gmail.com>
2022-03-06 00:12:56 -0600

    Second Half Level Blockout and Background Platforms

---
+++ Boomerang/Assets/Scripts/SemisolidPlatform.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SemisolidPlatform : MonoBehaviour
+{
+    private GameObject player;
+    private BoxCollider2D boxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player != null)
+        {
+            float playerHeight = 1.5F;
+            if(player.GetComponentInChildren<PlayerAnimation>().getAnimState() == PlayerAnimation.AnimationState.roll)
+                playerHeight = 1F;
+            playerHeight *= player.transform.localScale.y;
+            float playerWidth = 1F * player.transform.localScale.x;
+            float pBottom = player.transform.position.y - (playerHeight / 2F);
+            float pRight = player.transform.position.x + playerWidth / 2F;
+            float pLeft = player.transform.position.x - playerWidth / 2F;
+            float top = transform.position.y + transform.localScale.y / 2F;
+            float bottom = transform.position.y - transform.localScale.y / 2F;
+            if(pBottom > bottom && pBottom < top && pRight > transform.position.x - transform.localScale.x / 2 && pLeft < transform.position.x + transform.localScale.x / 2)
+                player.transform.position = new Vector3(player.transform.position.x, top + 0.01F + (playerHeight / 2F), player.transform.position.z);
+            if(pBottom < top)
+                boxCollider.isTrigger = true;
+            else
+                boxCollider.isTrigger = false;
+        }
+        else
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+}
