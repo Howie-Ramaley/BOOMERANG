52339cd2a5a44b08a39747ff850f1c7d7f998a60
Mitchell Ross <mross3143@gmail.com>
2022-04-05 10:36:06 -0500

    Implement tutorial text

---
+++ Boomerang/Assets/Scripts/TutorialText.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialText : MonoBehaviour
+{
+
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(transform.position.x - GameObject.FindGameObjectWithTag("Player").transform.position.x) <= 5 && GetComponent<TextMesh>().color.a <= 0.01f)
+        {
+            StartCoroutine(FadeTextToFullAlpha(1f, GetComponent<TextMesh>()));
+        }else if (Mathf.Abs(transform.position.x - GameObject.FindGameObjectWithTag("Player").transform.position.x) > 5 && GetComponent<TextMesh>().color.a >= 0.99f)
+        {
+            StartCoroutine(FadeTextToZeroAlpha(1f, GetComponent<TextMesh>()));
+        }
+    }
+
+    public IEnumerator FadeTextToFullAlpha(float t, TextMesh i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+ 
+    public IEnumerator FadeTextToZeroAlpha(float t, TextMesh i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+}
