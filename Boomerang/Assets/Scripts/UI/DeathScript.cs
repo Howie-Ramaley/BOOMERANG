ae86bb94d49925bae5318fff7d37233a1d7b05a4
Wharfiggle <elijahsouthman@gmail.com>
2022-04-26 02:56:35 -0500

    Fixes

--- Boomerang/Assets/Scripts/UI/DeathScript.cs
+++ Boomerang/Assets/Scripts/UI/DeathScript.cs
@@ -12,18 +12,26 @@ public class DeathScript : MonoBehaviour
 
     private int deaths;
 
+    private int wait;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        wait = 0;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(player.GetComponent<PlayerHealth>().getHealth() <= 0)
+        if(player.GetComponent<PlayerHealth>().getDied() && wait == 0)
         {
             deaths += 1;
+            wait = 2;
+        }
+        else if(wait > 0)
+        {
+            wait--;
         }
         deathCounter.text = "Deaths: " + deaths;
     }
