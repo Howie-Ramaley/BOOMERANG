e012f2066dd10f4b8a6bc27400c7eaf39790159a
Wharfiggle <elijahsouthman@gmail.com>
2022-04-26 02:18:55 -0500

    Lots of shit

--- Boomerang/Assets/Scripts/Stage/Parallax.cs
+++ Boomerang/Assets/Scripts/Stage/Parallax.cs
@@ -6,6 +6,7 @@ public class Parallax : MonoBehaviour
 {
     private Transform back1;
     private Transform back2;
+    private Transform front1;
     private Transform cam;
 
     // Start is called before the first frame update
@@ -13,6 +14,7 @@ public class Parallax : MonoBehaviour
     {
         back1 = transform.Find("Back1");
         back2 = transform.Find("Back2");
+        front1 = transform.Find("Front1");
         cam = GameObject.FindGameObjectWithTag("MainCamera").transform;
     }
 
@@ -25,6 +27,10 @@ public class Parallax : MonoBehaviour
         if(back2 != null)
         {
             back2.position = new Vector3 (cam.position.x * 0.3F, cam.position.y * 0.1F, back2.position.z);
+        }
+        if(front1 != null)
+        {
+            front1.position = new Vector3 (cam.position.x / -0.25F, front1.position.y, front1.position.z);
         }
     }
 }
